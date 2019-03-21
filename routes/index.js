ec010aa6fda51df28d50ce60d7a24cd5115a95f1
Nicola Solo <solo.nicola@live.com>
2019-03-21 03:45:04 +0200

    Aggiungere i file di progetto.

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET backend page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Airin love for Children' });
+});
+
+module.exports = router;
